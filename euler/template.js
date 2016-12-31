8a34fc7907e5ecde383983f8bd655d7c5dcc5a59
James Pfeiffer <jamesrpfeiffer@gmail.com>
2016-12-31 08:24:47 -0800

    Add scaffold for programming problems in Javascript.

---
+++ euler/template.js
@@ -0,0 +1,13 @@
+// Template file for Project Euler solutions.
+
+(function euler000() {
+  var answer = 0;
+
+  // Do work here ...
+  answer = "figgy";
+
+  // Debugging log statements...
+  console.log("Got to here");
+
+  document.write(answer);
+}());
